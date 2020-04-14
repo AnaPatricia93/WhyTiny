bf4a44acbaeab32ecd50e6c1057b8f292ee79bdc
Ana Graca <ibm.gts.mws.i00266@ibm.com>
2020-04-14 16:11:32 +0100

    Navbar and About page creation

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button, Form } from 'react-bootstrap';
 import Home from '../pages/home/Home';
 import About from '../pages/about/About';
 
@@ -9,13 +9,19 @@ export default class RouterComponent extends React.Component {
         return (
             <Router>
                 <Navbar bg="light" expand="lg">
+                    <Navbar.Brand className="navbar-brand" href="/">WhyTiny</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/about">About</Nav.Link>
                         </Nav>
+                        <Form inline>
+                            <Button variant="outline-info">Log In</Button>
+                        </Form>
                     </Navbar.Collapse>
                 </Navbar>
+
                 <Switch>
                     <Route exact path="/">
                         <Home />
